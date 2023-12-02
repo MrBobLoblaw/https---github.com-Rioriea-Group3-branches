c53cd73bacb8968bc83175e307f4b3e1b52a34d6
MrBobLoblaw <divern@outlook.com>
2023-12-01 22:46:23 -0500

    Revert "UserProfile Changes and Database config"
    This reverts commit e1c338b33581edd5f03f4c6e33db9d8667ab581d.

--- client/src/UserProfile.jsx
+++ client/src/UserProfile.jsx
@@ -6,7 +6,6 @@ import NavigationBar from './NavigationBar'
 
 function UserProfile() {
   const [users, setUsers] = useState([]);
-  const [surveys, setSurveys] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:3000/api/users')
@@ -16,40 +15,6 @@ function UserProfile() {
       })
       .catch((err) => console.log(err));
   }, []);
-  useEffect(() => {
-    axios.get('http://localhost:3000/api/surveys')
-      .then((response) => {
-        console.log(response.data);
-        setSurveys(response.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  let userSurveys = [];
-  users.forEach((user, uIndex) => {
-    if (user.id) { // If user id matches
-
-      surveys.forEach((survey, sIndex) => {
-
-        if (survey.userId == user.id) { // If survey's userId matches user's id
-
-          userSurveys.push(
-            <li>
-              <div>
-                <h3>{survey.name + " (" + sIndex + ")"}</h3>
-                <button id="Read">View</button>
-                <button id="Update">Edit</button>
-                <button id="Delete">Remove</button>
-              </div>
-            </li>
-          );
-
-        }
-    
-      });
-
-    }
-  });
 
   return (
     <div>
@@ -62,11 +27,8 @@ function UserProfile() {
           </div>
         ))
       }
-      <h3>Surveys: </h3>
-      <ul>{userSurveys}</ul>
     </div>
   );
-
 }
 
 export default UserProfile;
